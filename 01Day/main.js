9c272b60a88e4610faa1e7fbac6aa40d5cdd7d6e
Asif <asif16907@gmail.com>
2024-07-27 16:57:56 +0530

    Day1 Completed

---
+++ 01Day/main.js
@@ -0,0 +1,43 @@
+// Activity 1
+var num1 = 10
+console.log(num1)
+
+let num2 = 12
+console.log(num2)
+
+
+// Activity 2
+const HOLIDAY = true
+console.log(HOLIDAY);
+
+// Activity 3
+let var1 = 2
+let var2 = 'Asif'
+let var3 = true
+let var4 = {
+    'name':'Asif',
+    'lang':'JS'
+}
+let var5 = [1,2,3,4,5]
+
+console.log(typeof var1);
+console.log(typeof var2);
+console.log(typeof var3);
+console.log(typeof var4);
+console.log(typeof var5);
+
+
+// Activity 4
+let num = 2
+console.log(num);
+num = 4
+console.log(num);
+
+
+// Activity 5
+const PI = 3.14
+// PI = 1.34 --> results in Error
+// TypeError: Assignment to constant variable.
+
+
+// Activity 2
